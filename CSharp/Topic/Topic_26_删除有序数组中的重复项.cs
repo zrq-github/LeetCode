b3c2a26764c3a9b63bbba7239949f1e7d28d8407
zrq <wyzrq163@163.com>
2024-04-06 09:53:40 +0800

    26. 删除有序数组中的重复项

--- CSharp/Topic/Topic_26_删除有序数组中的重复项.cs
+++ CSharp/Topic/Topic_26_删除有序数组中的重复项.cs
@@ -1,10 +1,35 @@
-﻿namespace PerDayOneTopic;
+﻿namespace Topic;
 
 public class Solution
 {
-    public int RemoveDuplicates(int[] nums)
+    /// <summary>
+    /// 26. 删除有序数组中的重复项
+    /// </summary>
+    public int RemoveDuplicates(int[] nums)
     {
+        if(nums.Length == 1)
+        {
+            return 1;
+        }
 
-        return 0;
+        var p1 = 0;
+        var p2 = 1;
+        var total = 1;
+
+        while (p2 < nums.Length)
+        {
+            if (nums[p1] != nums[p2])
+            {
+                p1++;
+                nums[p1] = nums[p2];
+                total++;
+            }
+            else
+            {
+                p2++;
+            }
+        }
+
+        return total;
     }
 }
