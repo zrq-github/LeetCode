fb4b7b9304622683cf72e498cdba09db5f49313d
zrq <wyzrq163@163.com>
2024-04-10 17:09:53 +0800

    169_多数元素

---
+++ CSharp/Topic/Topic_169_多数元素.cs
@@ -0,0 +1,23 @@
+﻿namespace Topic;
+
+public class Topic_169_多数元素
+{
+    public int MajorityElement(int[] nums)
+    {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        foreach (int num in nums)
+        {
+            dic.TryAdd(num, 0);
+            dic[num]++;
+        }
+
+        foreach (var item in dic)
+        {
+            if(item.Value > nums.Length/2) 
+            {
+                return item.Key;
+            }
+        }
+        return 0;
+    }
+}
