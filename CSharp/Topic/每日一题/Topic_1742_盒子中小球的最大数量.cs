7bc6d200784f0c7b7cd6a79e723240912ca24db7
zrq <wyzrq163@163.com>
2025-02-13 23:57:43 +0800

    Topic_1742_盒子中小球的最大数量

---
+++ CSharp/Topic/每日一题/Topic_1742_盒子中小球的最大数量.cs
@@ -0,0 +1,32 @@
+﻿namespace Topic.每日一题;
+
+public class Topic_1742_盒子中小球的最大数量 
+{
+    public class Solution {
+        public int CountBalls(int lowLimit, int highLimit)
+        {
+            IDictionary<int, int> map = new Dictionary<int, int>();
+            int res = 0;
+
+            for (int i = lowLimit; i <= highLimit; i++)
+            {
+                int box = 0;
+                int x = i;
+                
+                // 计算int数字和
+                while (x !=0 )
+                {
+                    box += x % 10;
+                    x /= 10;
+                }
+                
+                map.TryAdd(box, 0);
+                map[box]++;
+                res = Math.Max(res, map[box]);
+            }
+            return res;
+
+            return 0;
+        }
+    }
+}
